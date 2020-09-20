1e703eedd065da6ec91f5b5e182381561247cc27
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-20 12:05:12 -0500

    navbar component completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="app">
+                <Header />
+                <Switch>
+                    <Route path="/checkout">Checkout</Route>
+                    <Route path="/login">Login Page</Route>
+                    <Route path="/">Home Page</Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <div className="header">
+            <Link to="/">
+                <img
+                    className="header__logo"
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                    alt=""
+                />
+            </Link>
+
+            <div className="header__search">
+                <input className="header__searchInput" type="search" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+            <div className="header__nav">
+                <Link className="header__link" to="/login">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">
+                            Hello Paulo
+                        </span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+            </div>
+            <div className="header__nav">
+                <Link className="header__link" to="/">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Order</span>
+                    </div>
+                </Link>
+            </div>
+            <div className="header__nav">
+                <Link className="header__link" to="/">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+            </div>
+            <Link className="header__link" to="/checkout">
+                <div className="header__optionBasket">
+                    <ShoppingBasket />
+                    <span className="header__optionLineTwo header__basketCount">
+                        0
+                    </span>
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export default Header;
