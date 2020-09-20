2110c0f7b9a3b7308dbe01815bda20af1a3cafc2
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-20 17:53:02 -0500

    created the homepage component and the main banner

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home";
 
 function App() {
     return (
         <Router>
             <div className="app">
                 <Header />
+                {/* <Home /> */}
                 <Switch>
                     <Route path="/checkout">Checkout</Route>
                     <Route path="/login">Login Page</Route>
-                    <Route path="/">Home Page</Route>
+                    <Route path="/" component={Home} />
                 </Switch>
             </div>
         </Router>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.scss";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <img
+                className="home__image"
+                src="https://images-eu.ssl-images-amazon.com/images/G/01/kindle/journeys/MTc0Y2M5Y2Mt/MTc0Y2M5Y2Mt-ZTMzMTk5Yjct-w1500._CB406837584_.jpg"
+                alt="homepage banner"
+            />
+        </div>
+    );
+};
+
+export default Home;
